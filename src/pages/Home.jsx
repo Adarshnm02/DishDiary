d081f65c289ccdbcd129813350e2560064afcd21
Adarsh nm <adarshnm02@gmail.com>
2024-12-18 19:50:18 +0530

    Create HeroSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../UIComponents/NavBar'
+import HeroSection from '../UIComponents/HeroSection'
 
 const Home = () => {
 
   return (
     <div>
         <Navbar/>
+        <HeroSection/>
     </div>
   )
 }
